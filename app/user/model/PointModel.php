a58c12879d1d8e6605ac8508460592624a230b33
Egist <maysrp@gmail.com>
2017-10-28 15:45:43 +0800

    point model changer

--- app/user/model/PointModel.php
+++ app/user/model/PointModel.php
@@ -25,15 +25,47 @@ class PhotoModel extends Model{
     public function point_action($info){
         $insert['uid']=$info['uid'];
         $now=$this->point_now($add);
-        if($info['am']){
-            $insert['point']=$now['point']+$info['point'];
-            $insert['am']=1;            
-        }else{
-            $insert['point']=$now['point']-$info['point'];
-            $insert['am']=0;            
+        if($now){
+            if($info['am']){
+                $insert['point']=$now['point']+$info['action'];
+                $insert['am']=1;            
+            }else{
+                $insert['point']=$now['point']-$info['action'];
+                $insert['am']=0;            
+            }
+            $insert['time']=time();
+            $insert['action']=$info['action'];
+            return $this->insert($insert);
         }
-        $insert['time']=time();
-        return $this->insert($insert);
+    }
+    public function point_del($info){//true為成功 添加一個無action值的表示為取消上次分數
+        $insert['uid']=$info['uid'];
+        $now=$this->point_now($add);
+        $where['pid']=$info['pid'];
+        $where['del']=0;
+        $delinfo=$this->where($where)->find();
+        if($now&&$delinfo->data){
+            $update['pid']=$info['pid'];
+            $update['del']=0;
+            $update['time']=time();
+            $rem=$this->update($update);
+            if($rem){
+                $insert['time']=time();
+                $insert['uid']=$info['uid'];
+                $delone=$delinfo->data;
+                if($delone['am']){
+                    $insert['point']=$now['point']+$delone['action'];
+                }else{
+                    $insert['point']=$now['point']-$delone['action'];
+                }
+                $insert['del']=$delone['pid'];//保留上次刪除的pid
+                return $this->insert($insert);
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 
 
